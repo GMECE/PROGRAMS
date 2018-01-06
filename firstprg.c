4d4da66c45ec7e4f37c879467b71b055c50864b7
LavanyaPalanisamy <plavanya6896@gmail.com>
2018-01-06 20:21:39 +0530

    FC

---
+++ firstprg.c
@@ -0,0 +1,33 @@
+#include"stdio.h"
+#include"conio.h"
+int main()
+{
+int a,b,c;
+clrscr();
+printf("\n enter a,b");
+scanf("%d %d",&a,&b);
+c=a+b;
+printf("\n %d",c);
+getch();
+return(0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
